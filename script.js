b4fd16513b5055280d4a0879da41e43442a78c0e
Jamal Shem Philips <pjamal.jp@gmail.com>
2023-11-24 21:47:26 +0300

    new files

---
+++ script.js
@@ -0,0 +1,12 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementByI('navbar')
+
+if (bar) {
+    bar.addEventListener('click', () => {
+    nav.classList.add('active')    
+    })
+}
+
+{
+    "editor.formatOnSave" ;true;
+}
